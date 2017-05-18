dd9a1d9e5ab5ec3673f94b8b818a1f5d20f9efa9
Lars Olav Kjølstad <larsolavk@gmail.com>
2017-05-18 21:18:36 +0200

    Initial commit

---
+++ dotnet2-arm/Build.ps1
@@ -0,0 +1,13 @@
+﻿Write-Host Building runtime dependencies
+
+Push-Location -Path $PSScriptRoot\runtime-deps
+&docker build --rm --force-rm -t larsolavk/dotnet-arm:2.0-runtime-deps .
+Pop-Location
+
+Push-Location -Path $PSScriptRoot\runtime
+&docker build --rm --force-rm -t larsolavk/dotnet-arm:2.0-runtime .
+Pop-Location
+
+Push-Location -Path $PSScriptRoot\aspnetcore
+&docker build --rm --force-rm -t larsolavk/aspnetcore-arm:2.0 .
+Pop-Location
---
+++ dotnet2-arm/Push.ps1
@@ -0,0 +1,2 @@
+﻿&docker push larsolavk/dotnet-arm:2.0-runtime-deps
+&docker push larsolavk/dotnet-arm:2.0-runtime
